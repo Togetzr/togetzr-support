1271d6e7c736f44bf1c06d5419e17f70fca19783
Togetzr <fkassab@togetzr.com>
2025-12-24 00:16:20 +0100

    Fix: Update RustdeskImpl to TogetzrSupport to match bridge generator

--- flutter/lib/models/native_model.dart
+++ flutter/lib/models/native_model.dart
@@ -32,7 +32,7 @@ class PlatformFFI {
   // _homeDir is only needed for Android and IOS.
   String _homeDir = '';
   final _eventHandlers = <String, Map<String, HandleEvent>>{};
-  late RustdeskImpl _ffiBind;
+  late TogetzrSupport _ffiBind;
   late String _appType;
   StreamEventHandler? _eventCallback;
 
@@ -41,7 +41,7 @@ class PlatformFFI {
   static final PlatformFFI instance = PlatformFFI._();
   final _toAndroidChannel = const MethodChannel('mChannel');
 
-  RustdeskImpl get ffiBind => _ffiBind;
+  TogetzrSupport get ffiBind => _ffiBind;
   F3? _session_get_rgba;
 
   static get localeName => Platform.localeName;
@@ -139,7 +139,7 @@ class PlatformFFI {
       } catch (e) {
         debugPrint('Failed to get documents directory: $e');
       }
-      _ffiBind = RustdeskImplImpl(dylib);
+      _ffiBind = TogetzrSupportImpl(dylib);
 
       if (isLinux) {
         if (isMain) {
@@ -239,7 +239,7 @@ class PlatformFFI {
   }
 
   /// Start listening to the Rust core's events and frames.
-  void _startListenEvent(RustdeskImpl rustdeskImpl) {
+  void _startListenEvent(TogetzrSupport rustdeskImpl) {
     final appType =
         _appType == kAppTypeDesktopRemote ? '$_appType,$kWindowId' : _appType;
     var sink = rustdeskImpl.startGlobalEventStream(appType: appType);
--- flutter/lib/models/platform_model.dart
+++ flutter/lib/models/platform_model.dart
@@ -1,12 +1,12 @@
 import 'native_model.dart' if (dart.library.html) 'web_model.dart';
-// RustdeskImpl is imported via native_model.dart or web_model.dart
-import 'package:flutter_hbb/generated_bridge.dart' show RustdeskImpl
+// TogetzrSupport is imported via native_model.dart or web_model.dart
+import 'package:flutter_hbb/generated_bridge.dart' show TogetzrSupport, TogetzrSupportImpl
     if (dart.library.html) 'package:flutter_hbb/web/bridge.dart' show RustdeskImpl;
 
 final platformFFI = PlatformFFI.instance;
 final localeName = PlatformFFI.localeName;
 
-RustdeskImpl get bind => platformFFI.ffiBind;
+TogetzrSupport get bind => platformFFI.ffiBind;
 
 String ffiGetByName(String name, [String arg = '']) {
   return PlatformFFI.getByName(name, arg);
